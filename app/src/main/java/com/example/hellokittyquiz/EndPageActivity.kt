a36b11d8b72cf66effbfa38d60325a2d67430f36
ElliotFackler <99917625+ElliotFackler@users.noreply.github.com>
2022-03-15 18:32:23 -0500

    End Page files and strings created.

---
+++ app/src/main/java/com/example/hellokittyquiz/EndPageActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hellokittyquiz
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+
+class EndPageActivity {
+
+    private lateinit var showfinishbutton: Button
+}
