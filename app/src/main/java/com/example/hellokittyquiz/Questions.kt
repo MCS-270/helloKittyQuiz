b15442fbfcf5f2d0e3b60851af6ff643f72fe49b
ElliotFackler <99917625+ElliotFackler@users.noreply.github.com>
2022-03-15 19:18:35 -0500

    Imagebranch (#8)
    * End Page files and strings created.
    
    * End Page files and strings created.

--- app/src/main/java/com/example/hellokittyquiz/Questions.kt
+++ app/src/main/java/com/example/hellokittyquiz/Questions.kt
@@ -1,3 +1,4 @@
 import androidx.annotation.StringRes
+import androidx.annotation.DrawableRes
+data class Questions(@DrawableRes val imageResID: Int, @StringRes val textResID: Int, val answer: Boolean)
 
-data class Questions(@StringRes val textResID: Int, val answer: Boolean)
